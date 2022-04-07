20cc8b1001785a9a8769f1b76c418d8749908fc3
htmathl <matheus.domingues126@gmail.com>
2022-04-06 23:22:59 -0300

    organizar código

--- auau.js
+++
@@ -1,7 +0,0 @@
-var url = "https://docs.python-requests.org/pt_BR/latest/user/quickstart.html";
-
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", url, false);
-xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
-
-console.log(xhttp.responseText);
